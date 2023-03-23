f39c3f1ce21cb640bdbf8c452caa8a35aabf6bc7
Prema Shivarama <103142759+NoviceCoder17@users.noreply.github.com>
2023-03-22 23:51:48 -0400

    Update dbConnect.js

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 async function dbConnect(){
     try{
-        await mongoose.connect("mongodb+srv://premashivarama:Fwt5Eoy7Iqd8xguh@prema.jrf1mhl.mongodb.net/car-seller")
+        await mongoose.connect("mongodb+srv://car-seller")
         console.log("MongoDB is connected")
     }
     catch(error){
